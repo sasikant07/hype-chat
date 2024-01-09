b82af7f8474b8930c74040f957ea19f039d75452
Sasikant Biswal <biswalsashikant@gmail.com>
2024-01-10 01:27:53 +0530

    Register Login design page and redux initial setup

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="card-header">
+          <h3>Login</h3>
+        </div>
+        <div className="card-body">
+          <form>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                id="email"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                id="password"
+              />
+            </div>
+            <div className="form-group">
+              <input type="submit" value="login" className="btn" />
+            </div>
+            <div className="form-group">
+              <span>
+                Not a user? <Link to="/messenger/register">Register</Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
