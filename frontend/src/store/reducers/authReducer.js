37bb4f1ad390075e6e8653955ac2274b26fefd45
Sasikant Biswal <biswalsashikant@gmail.com>
2024-01-19 23:43:22 +0530

    user registration redux integration

--- frontend/src/store/reducers/authReducer.js
+++ frontend/src/store/reducers/authReducer.js
@@ -1,14 +1,31 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import api from "../../api/api";
+import { jwtDecode } from "jwt-decode";
 
-const initialState = {};
+const decodeToken = (token) => {
+  if (token) {
+    const userInfo = jwtDecode(token);
+
+    return userInfo;
+  } else {
+    return "";
+  }
+};
+
+const initialState = {
+  loading: false,
+  authenticate: false,
+  error: "",
+  success: "",
+  myInfo: decodeToken(localStorage.getItem("authToken")),
+};
 
 export const userRegister = createAsyncThunk(
   "auth/user-register",
   async (info, thunkAPI) => {
     try {
       const { data } = await api.post("/messenger/user-register", info);
-      // localStorage.setItem("customerToken", data.token);
+      localStorage.setItem("authToken", data.token);
       return thunkAPI.fulfillWithValue(data);
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data);
@@ -19,7 +36,30 @@ export const userRegister = createAsyncThunk(
 const authReducer = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    messageClear: (state, _) => {
+      state.error = "";
+      state.success = "";
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(userRegister.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(userRegister.fulfilled, (state, action) => {
+      state.loading = false;
+      state.success = action.payload.successMessage;
+      const myInfo = decodeToken(action.payload.token);
+      state.myInfo = myInfo;
+      state.authenticate = true;
+    });
+    builder.addCase(userRegister.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload.error.errorMessage;
+      state.authenticate = false;
+      state.myInfo = "";
+    });
+  },
 });
 
 export const {} = authReducer.actions;
